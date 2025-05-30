e21c664940cb9c5c6e6003be9039143add768a46
Cam Coulter <git@camcoulter.com>
2025-05-29 18:39:06 -0700

    Switch from JS to JSON data file

--- src/_data/meta.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	// NOTE: `process.env.URL` is provided by Netlify, and may need adjusting pending your host
-	url: "https://dozen.camcoulter.com",
-	siteName: "Dozen",
-	siteDescription: "A simple starter website for the 11ty static site generator. Hopefully this gets used to spin off a dozen or more websites.",
-};
