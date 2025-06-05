6ad2af0baab326835d5256950f6e81b075903c17
Cam Coulter <git@camcoulter.com>
2025-06-04 18:04:22 -0700

    Clean eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,36 @@
 const { DateTime } = require("luxon"); // Luxon is a JavaScript library for working with dates and times: https://moment.github.io/luxon/
 
 module.exports = function (eleventyConfig) {
-	// Copy through these files from src/ to public/ without modifying them.
+
+	// |------------------------------|
+	// | Passthroughs & Watch Targets |
+	// |------------------------------|
+
+	// Copy these files from src/ to public/ without modifying them.
 	eleventyConfig.addPassthroughCopy("./src/css/");
-	eleventyConfig.addPassthroughCopy("./src/*.txt");
+	eleventyConfig.addPassthroughCopy("./src/*.txt")
+
+	// Trigger a build when you edit these files.
+	eleventyConfig.addWatchTarget("./src/css/");
 	
-	// Date formatting (human readable)
-	// See: https://github.com/moment/luxon/blob/master/docs/formatting.md
-  	eleventyConfig.addFilter("humanReadable", dateObj => {
-		return DateTime.fromJSDate(dateObj).toFormat("d LLLL y h:mm a ZZZZ");
-  	});
+	// |-------------|
+	// | Collections |
+	// |-------------|
 
-	// Date formatting (for URLs)
-	// See: https://github.com/moment/luxon/blob/master/docs/formatting.md
-  	eleventyConfig.addFilter("URLDate", dateObj => {
-    	return DateTime.fromJSDate(dateObj).toFormat("y/LL/dd");
-  	});
-
-	// Date formatting (machine readable)
-	// See: https://github.com/moment/luxon/blob/master/docs/formatting.md
-  	eleventyConfig.addFilter("machineReadable", dateObj => {
-    	return DateTime.fromJSDate(dateObj).toISO();
-  	});
-
-	// Sets up {% year %}, which returns the current year
-	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-
-	// COLLECTIONS
-	// Creates the collection categorizedPages, which includes every page that has categorizedUnder in its frontmatter.
+	// Create the collection: categorizedPages
+	// which includes every page that has categorizedUnder in its frontmatter.
 	eleventyConfig.addCollection("categorizedPages", function (collection) {
 		return collection.getAll().filter((item) => item.data.categorizedUnder);
 	});
+
+	// Create the collection: taggedPages
+	// which includes every page that has taggedWith in its frontmatter.
 	eleventyConfig.addCollection("taggedPages", function (collection) {
 		return collection.getAll().filter((item) => item.data.taggedWith);
 	});
+
+	// Create the collection: categories
+	// which is an array of every category (from categorizedUnder front matter).
 	eleventyConfig.addCollection("categories", function (collection) {
 		const categorizedPages = collection.getAll().filter((item) => item.data.categorizedUnder);
 		const allCategories = [];
@@ -42,29 +39,15 @@ module.exports = function (eleventyConfig) {
 		});
 		let duplicatesRemoved = [...new Set(allCategories)];
 		return duplicatesRemoved.sort();
-	});
-
-	eleventyConfig.addFilter("listCategories", function(categorizedPages) {
-		// Given a collection where all items have item.data.categorizedUnder
-		// This filter will return an array of all categories.
-		// You likely want to use this filter like so: {% for item in collections.categorizedPages | listCategories %}
-		// That will let you iterate through each category.
-		const allCategories = [];
-		categorizedPages.forEach((page) => {
-			allCategories.push(...page.data.categorizedUnder);
-		});
-		let duplicatesRemoved = [...new Set(allCategories)];
-		return duplicatesRemoved.sort();
 		// To integrate capitals and lowercase, add (a, b) => a.localeCompare(b) within sort()
 	});
 
-	eleventyConfig.addFilter("listTags", function(taggedPages) {
-		// Given a collection where all items have item.data.taggedWith
-		// This filter will return an array of all tags.
-		// You likely want to use this filter like so: {% for item in collections.taggedPages | listTags %}
-		// That will let you iterate through each tag.
+	// Create the collection: tags
+	// which is an array of every tag (from taggedWith front matter).
+	eleventyConfig.addCollection("tags", function (collection) {
+		const categorizedPages = collection.getAll().filter((item) => item.data.taggedWith);
 		const allTags = [];
-		taggedPages.forEach((page) => {
+		categorizedPages.forEach((page) => {
 			allTags.push(...page.data.taggedWith);
 		});
 		let duplicatesRemoved = [...new Set(allTags)];
@@ -72,10 +55,46 @@ module.exports = function (eleventyConfig) {
 		// To integrate capitals and lowercase, add (a, b) => a.localeCompare(b) within sort()
 	});
 
+	// |---------|
+	// | Filters |
+	// |---------|
+
+	// Date formatting (human readable)
+	// See: https://github.com/moment/luxon/blob/master/docs/formatting.md
+  	// Given an ISO 8601 datetime value,
+	// Returns a human readable datevalue value.
+	// Example Use: {{ date | humanReadable }}
+	// Example Return: 5 June 2025 7:42 AM PDT
+	eleventyConfig.addFilter("humanReadable", dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat("d LLLL y h:mm a ZZZZ");
+  	});
+
+	// Date formatting (for URLs)
+	// See: https://github.com/moment/luxon/blob/master/docs/formatting.md
+	// Given an ISO 8601 datetime value,
+	// Returns year/month/day.
+	// Example Use: {{ date | URLDate }}
+	// Example Return: 2025/06/05
+  	eleventyConfig.addFilter("URLDate", dateObj => {
+    	return DateTime.fromJSDate(dateObj).toFormat("y/LL/dd");
+  	});
+
+	// Date formatting (machine readable)
+	// See: https://github.com/moment/luxon/blob/master/docs/formatting.md
+	// Given an ISO 8601 datetime value,
+	// Returns an ISO 8601 datetime value.
+	// Example Use: {{ date | machineReadable }}
+	// Example Return: 2025-06-05T07:42:00.000-07:00
+  	eleventyConfig.addFilter("machineReadable", dateObj => {
+    	return DateTime.fromJSDate(dateObj).toISO();
+  	});
+
+	// Create the filter: categorizedUnder
+	// Given collections.categorizedPages and a category
+	// Returns an array with all pages that are categorizedUnder that category.
+	// Example Use: {% for item in collections.categorizedPages | categorizedUnder("web design") %}
+	// Example Return: an array of all pages that are categorizedUnder "web design"
 	eleventyConfig.addFilter("categorizedUnder", function(categorizedPages, category) {
-		// Given collections.categorizedPages & a category
-		// This filter will return an array of all pages that are categorizedUnder that category.
-		// Example usage: {% for item in collections.categorizedPages | categorizedUnder("web design") %}
 		const relevantPages = [];
 		categorizedPages.forEach((page) => {
 			if ([...page.data.categorizedUnder].includes(category)) {
@@ -85,10 +104,12 @@ module.exports = function (eleventyConfig) {
 		return relevantPages;
 	});
 
+	// Create the filter: taggedWith
+	// Given collections.taggedPages and a tag
+	// Returns an array with all pages that are taggedWith that tag.
+	// Example Use: {% for item in collections.taggedPages | taggedWith("web design") %}
+	// Example Return: an array of all pages that are taggedWith "web design"
 	eleventyConfig.addFilter("taggedWith", function(taggedPages, tag) {
-		// Given collections.taggedPages & a tag
-		// This filter will return an array of all pages that are taggedWith that tag.
-		// Example usage: {% for item in collections.taggedPages | taggedWith("web design") %}
 		const relevantPages = [];
 		taggedPages.forEach((page) => {
 			if ([...page.data.taggedWith].includes(tag)) {
@@ -98,8 +119,12 @@ module.exports = function (eleventyConfig) {
 		return relevantPages;
 	});
 
+	// Create the filter: authorIs
+	// Given a collection of pages and a code_name
+	// Returns an array with all pages in that collection that have author set to that code_name in their front matter.
+	// Example Use: {% for item in collections.post | authorIs(code_name) %}
+	// Example Return: an array of all pages that have author: code_name
 	eleventyConfig.addFilter("authorIs", function(collection, code_name) {
-		// Returns an array of all items that the author wrote
 		const relevantPages = [];
 		collection.forEach((page) => {
 			if (page.data.author == code_name) {
@@ -109,6 +134,11 @@ module.exports = function (eleventyConfig) {
 		return relevantPages;
 	});
 	
+	// Create the filter: personIs
+	// Given collections.people and a code_name
+	// Returns an array with all pages in collections.people that have code_name set to given code_name in their front matter. (Ideally, it returns an array of one. If the array includes multiple pages, ensure all code_names are unique.)
+	// Example Use: {% for item in collections.people | personIs(author) %}
+	// Example Return: an array of all pages that have author: code_name (ideally an array of one)
 	eleventyConfig.addFilter("personIs", function(people, author) {
 		const relevantPages = [];
 		people.forEach((person) => {
@@ -119,6 +149,18 @@ module.exports = function (eleventyConfig) {
 		return relevantPages;
 	});
 
+	// |------------|
+	// | Shortcodes |
+	// |------------|
+
+	// Create the shortcode: {% year %}
+	// which returns the current year.
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	// |--------|
+	// | Return |
+	// |--------|
+	
 	return {
 		markdownTemplateEngine: 'njk',
 		dataTemplateEngine: 'njk',
